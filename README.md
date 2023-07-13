46c0999c6951d089ac386391ffd49809a92164b3
gustaveWPM <jgeorge@perso.be>
2023-07-13 15:33:26 +0200

    review: splitted code, some sanitizing, keyboard events...

---
+++ README.md
@@ -0,0 +1,3 @@
+# Run me!
+
+`$ python3 pouce_to_cm_converter.py`
---
+++ inches_to_centimeters_converter.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+
+
+INCHES_TO_CENTIMETERS_FACTOR = 2.54
+CENTIMETERS_TO_INCHES_FACTOR = 0.394
+
+VOCAB = {
+    "WELCOME_MSG": "Hello world!\n",
+    "CONVERSION_PROMPT_MSG": "Please, choose a conversion option:",
+    "INVALID_INPUT": "Invalid input!",
+    "CENTIMETERS": "Centimeters",
+    "INCHES": "Inches",
+    "OPTIONS": [
+        "'A' ->\tConvert inches to centimeters,",
+        "'B' ->\tConvert centimeters to inches,",
+        "'C' ->\tQuit",
+    ],
+    "QUIT_MSG": "Goodbye!",
+}
+
+
+def input_handling_keyboard_interrupt_wrapper(input_msg: str) -> str:
+    try:
+        user_input = input(input_msg)
+        return user_input
+    except (KeyboardInterrupt, EOFError):
+        print(f"\n{VOCAB['QUIT_MSG']}")
+        exit(0)
+
+
+def pause():
+    input()
+
+
+def display_result(unit: str, res: float):
+    print(f"Converted to {unit.lower()}:\n> {res}")
+
+
+def value_prompt(unit: str):
+    return f"{unit}:\n> "
+
+
+def sanitize_user_input(user_input: str) -> str:
+    return user_input.strip().lower()
+
+
+def is_number(unknown_value) -> bool:
+    try:
+        float(unknown_value)
+        return True
+    except ValueError:
+        return False
+
+
+def inches_to_centimeters() -> float:
+    user_input = input_handling_keyboard_interrupt_wrapper(
+        value_prompt(VOCAB["INCHES"])
+    )
+    user_input = sanitize_user_input(user_input)
+    if not is_number(user_input):
+        print(f"\n{VOCAB['INVALID_INPUT']}")
+        inches_to_centimeters()
+    else:
+        res = round(float(user_input) * INCHES_TO_CENTIMETERS_FACTOR, 3)
+        return res
+
+
+def centimeters_to_inches() -> float:
+    user_input = input_handling_keyboard_interrupt_wrapper(
+        value_prompt(VOCAB["CENTIMETERS"])
+    )
+    user_input = sanitize_user_input(user_input)
+    if not is_number(user_input):
+        print(f"\n{VOCAB['INVALID_INPUT']}")
+        centimeters_to_inches()
+    else:
+        res = round(float(user_input) * CENTIMETERS_TO_INCHES_FACTOR, 3)
+        return res
+
+
+def prompt():
+    QUIT_CHOICE = "c"
+
+    while True:
+        print(VOCAB["CONVERSION_PROMPT_MSG"])
+        print(*VOCAB["OPTIONS"], sep="\n")
+        user_input = input_handling_keyboard_interrupt_wrapper("> ")
+        user_input = sanitize_user_input(user_input)
+        res = 0
+        if user_input == "a":
+            res = inches_to_centimeters()
+            display_result(VOCAB["CENTIMETERS"], res)
+        elif user_input == "b":
+            res = centimeters_to_inches()
+            display_result(VOCAB["INCHES"], res)
+        elif user_input == QUIT_CHOICE:
+            print(VOCAB["QUIT_MSG"])
+            break
+        else:
+            print(f"\n{VOCAB['INVALID_INPUT']}")
+            prompt()
+
+        if user_input != QUIT_CHOICE:
+            pause()
+
+
+def runtime():
+    print(VOCAB["WELCOME_MSG"])
+    prompt()
+
+
+if __name__ == "__main__":
+    runtime()
